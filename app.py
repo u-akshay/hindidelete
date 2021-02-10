f1ca3bf80c95c71cbc699f095a029dfd977929c7
u-akshay <56358614+u-akshay@users.noreply.github.com>
2021-02-10 22:25:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from textblob import TextBlob
+
+from pyrogram import Client
+from os import environ
+
+api_id = int(environ["API_ID"])
+api_hash = environ["API_HASH"]
+bot_token = environ["BOT_TOKEN"]
+
+app = Client("my_bot",
+             api_id=api_id,
+             api_hash=api_hash,
+             bot_token=bot_token)
+
+# language = 'hi'
+
+@app.on_message()
+def message(client, message):
+    lang = TextBlob(message.text)
+    if (lang.detect_language) == "hi":
+        # app.delete_messages(chat_id, message_id)
+        print(message)
+app.run()
+
